7cfb2cda8352f689a7b526a4002f5b8ecdf1b670
vineetkumardoshi <vineetvivid@gmail.com>
2014-06-08 13:48:59 +0530

    initial commit

---
+++ Chapter 2/IsArrayInSortedOrder/IsArrayInSortedOrder.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+int *A;
+
+int IsArrayInSortedOrder(int A[],int n){
+  if(n == 1)
+    return 1;
+  return (A[n-1]<A[n-2])?0:IsArrayInSortedOrder(A,n-1);
+}
+
+int main(){
+  
+  int n;
+  
+  cout<<"Enter the length of Array: ";
+  cin>>n;
+  
+  A = new int[n];
+  for(int i=0; i<n; i++){
+    cout<<"Enter element number "<<i+1<<" of Array: ";
+    cin>>A[i];
+  }
+  
+  int answer = IsArrayInSortedOrder(A,n);
+  
+  cout<<answer;
+  
+  return 0;
+}
---
+++ Chapter 2/IsArrayInSortedOrder/README.md
@@ -0,0 +1,9 @@
+Is Array In Sorted Order ?
+==========================
+
+Prints 0 if Array elements are sorted in ascending order.
+Otherwise, prints 1.
+
+Uses recursive algorithm.
+Time Complexity = O(n)
+Space Complexity = O(n)
